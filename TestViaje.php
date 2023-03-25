3642ad72767507beee56c994064d1b26312c7ed6
Gonzalo <gonzalo@estadodigital.com>
2023-03-25 16:46:50 -0300

    Inicio TestViaje

---
+++ TestViaje.php
@@ -0,0 +1,10 @@
+<?php
+
+include('./Viaje.php');
+echo "\n\n\n";
+$separador = "###########################\n";
+echo $separador;
+echo "INICIO TEST_VIAJE\n";
+echo $separador;
+echo "\n";
+/* Iniciamos los TEST */
